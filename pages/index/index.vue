587e31c4075b79aefa053a3f60ca8d069dc0c708
yqcncas <yqcncas@163.com>
2020-08-11 17:31:13 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,156 @@
+<template>
+	<view class="index">
+		<view class="index-top">
+			<image src="../../static/image/banner.png" mode=""></image>
+		</view>
+		<view class="index-center">
+			<view class="index-center-top">
+				<image src="../../static/image/logo.png" mode=""></image>
+			</view>
+			<view class="index-center-bottom">趣砍App</view>
+		</view>
+		<view class="index-bottom">
+			<view class="index-bottom-top" @click="handleIosDown">
+				<image src="../../static/image/pingguo.png" mode=""></image>
+				<view class="index-bottom-top-name">立即下载ios版</view>
+			</view>
+			<view class="index-bottom-footer" @click="handleAndDown">
+				<image src="../../static/image/anzhuo-2.png" mode=""></image>
+				<view class="index-bottom-footer-name">立即下载安卓版</view>
+			</view>
+		</view>
+		<tip-open-browser :openBrowserFlag="openBrowserFlag"></tip-open-browser>
+	</view>
+</template>
+
+<script>
+	import tipOpenBrowser from './open-browser'
+	export default {
+		data() {
+			return {
+				openBrowserFlag: false,
+				androidDown: 'http://qukan-h5.bajiaostar.com/qukan.apk',
+				iosDown: 'https://apps.apple.com/cn/app/gg%E6%9D%A5%E9%80%81/id1494559484',
+			}
+		},
+		components: {
+		    tipOpenBrowser
+		},
+		onLoad() {
+			this.getDownPath()
+		},
+		methods: {
+			getDownPath() {
+				if(/android/i.test(navigator.userAgent)){
+				   //Android平台下浏览器
+				    if (this.isWeiXin()) this.openBrowserFlag = true 
+					else {
+						 this.openBrowserFlag = false
+						 // window.location.href = this.androidDown
+					}
+				   
+				}
+			},
+			// 判断是否微信浏览器
+			isWeiXin() {
+				var ua = window.navigator.userAgent.toLowerCase()
+				if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+					return true
+				} else {
+					return false
+				}
+			},
+			// ios下载
+			handleIosDown () {
+				uni.showToast({
+					icon: 'none',
+					title: '暂未开通'
+				})
+			},
+			handleAndDown () {
+				window.location.href = this.androidDown
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	.index{
+		.index-top{
+			width: 100%;
+			height: 476rpx;
+			position: relative;
+			image{
+				width: 100%;
+				height: 100%;
+			}
+		}
+		.index-center{
+			width: 100%;
+			text-align: center;
+			position: absolute;
+			top: 25.3%;
+			.index-center-top{
+				
+				image{
+					width: 200rpx;
+					height: 200rpx;
+					box-sizing: border-box;
+					box-shadow: 0 2px 10px 0 rgba(0,0,0,0.10);
+				}
+			}
+			.index-center-bottom{
+				padding-top: 50rpx;
+				box-sizing: border-box;
+				font-family: PingFangSC-Medium;
+				font-size: 22px;
+				color: #000000;
+				font-weight: bold;
+			}
+		}
+		.index-bottom{
+			width: 100%;
+			text-align: center;
+			margin: 0 auto;
+			position: absolute;
+			bottom: 262rpx;
+			// left: 50%;
+			// transform: translateX(-50%);
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			image{
+				width: 42rpx;
+				height: 42rpx;
+				margin-right: 20rpx;
+				box-sizing: border-box;
+			}
+			.index-bottom-top, .index-bottom-footer{
+				width: 540rpx;
+				height: 100rpx;
+				display: flex;
+				text-align: center;
+				justify-content: center;
+				align-items: center;
+				background-image: linear-gradient(180deg, #FF7360 0%, #C9281C 100%);
+				border-radius: 25px;
+				font-family: PingFangSC-Regular;
+				font-size: 17px;
+				color: #FFFFFF;
+				letter-spacing: -0.41px;
+				text-align: justify;
+			}
+			.index-bottom-footer{
+				margin-top: 50rpx;
+				box-sizing: border-box;
+				image{
+					transform: translateX(8rpx);
+				}
+				.index-bottom-footer-name{
+					transform: translateX(8rpx);
+				}
+			}
+		}
+	}
+</style>
---
+++ pages/index/open-browser.vue
@@ -0,0 +1,92 @@
+<template>
+	<view class="container-tip" :class="openBrowserFlag ? 'show' : ''">
+		<image class="bg" src="/static/tip-open-browser.png"></image>
+		<view class="tip-text">
+			<view>1</view>
+			<text>点击右上角的按钮</text>
+		</view>
+		<view class="tip-text-2">
+			<view class="tip-text-2-num">2</view>
+			<text>选择在浏览器中打开</text>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	props: ['openBrowserFlag']
+};
+</script>
+
+<style lang="less" scoped="scoped">
+.container-tip {
+	position: fixed;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	z-index: 1110;
+	opacity: 0;
+	outline: 0;
+	text-align: center;
+	-ms-transform: scale(1.185);
+	transform: scale(1.185);
+	backface-visibility: hidden;
+	perspective: 2000upx;
+	background: rgba(0, 0, 0, 0.6);
+	transition: all 0.3s ease-in-out 0s;
+	pointer-events: none;
+
+	.bg {
+		width: 90upx;
+		height: 144upx;
+		position: fixed;
+		right: 30upx;
+		top: 30upx;
+	}
+
+	.tip-text,
+	.tip-text-2 {
+		display: flex;
+		align-items: center;
+		position: fixed;
+		color: #fff;
+		right: 62upx;
+		top: 180upx;
+
+		view:first-child {
+			width: 36upx;
+			height: 36upx;
+			background: #FF3854;
+			border-radius: 50%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		.tip-text-2-num{
+			transform: translateX(9rpx);
+		}
+
+		text:last-child {
+			margin-left: 30upx;
+			letter-spacing: 4upx;
+			text-align: justify;
+		}
+	}
+
+	.tip-text-2 {
+		top: 250upx;
+		right: 30upx;
+	}
+
+	&.show {
+		opacity: 1;
+		transition-duration: 0.3s;
+		-ms-transform: scale(1);
+		transform: scale(1);
+		overflow-x: hidden;
+		overflow-y: auto;
+		pointer-events: auto;
+	}
+}
+</style>
